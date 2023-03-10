121245b081aefd86c8b59a77964baa0ee39dc635
Madhur Nutulapati <madhurnutulapati@gmail.com>
2023-03-09 20:10:48 -0500

    FINISHED RUN BATCH method.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # myshell
+
+Define a function to parse user input into a list of command and argument strings. This function should handle whitespace and quoted arguments correctly.
+
+If mysh is called with an argument, open the specified file and read its contents into a list of command strings.
+
+If mysh is not called with an argument, enter an input loop that prompts the user for input and calls the command parsing function on each line of input.
+
+For each command string, execute it by calling the appropriate system function (e.g., os.system()) with the command and argument strings as arguments.
+
+If a command is "exit", terminate mysh and return control to the calling program.
--- mysh.c
+++ mysh.c
@@ -1,7 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(){
+    //arbitrary value for the max command line characters
+    #define BUFF_SIZE 1024
+
+void runBatch(char *fName);
+
+int main(int argc, char *argv[]){   
+    if(argc==2){
+        //batchmode
+        //passes in file name to runBatch
+        runBatch(argv[1]);
+    }
+    if(argc==1){
+        //interactive mode
+    }
+    if(argc==0){
+        printf("No arguments passed in");
+        return EXIT_FAILURE;
+    }
+
     return 0; 
+}
+
+void runBatch(char *fName){
+    
+    int FD = open(fName,O_RDONLY);
+
+    if(FD==-1){//means there is an error in File directory 
+    printf("Error opening file - %s\n",fName);
+    exit(1);
+    }
+
+    ssize_t bytes;
+
+    char buffer[BUFF_SIZE];
+
+    /*parsing a sequence of commands that are separated 
+    by newline characters, and executing each command as soon as 
+    it is complete.
+    */
+    while((bytes=read(FD,buffer,BUFF_SIZE))>0){
+        for(long int j=0; j<bytes;j++){
+            if(buffer[j]=='\n'){
+                buffer[j]='\0';
+                //function call to parse the commands  
+                //right here
+
+                buffer[j]='\n';
+            }
+        }
+        write(STDOUT_FILENO, buffer, bytes);
+
+        //buffer command is the word exit
+        if (strcmp(buffer, "exit\n") == 0){break;}
+    }
+        
+    if (bytes == -1) {
+    perror("Error reading file");
+    exit(1);
+    }
+
+    close(FD);
 }
