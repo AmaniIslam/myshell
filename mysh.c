736f04042486b0e057f77432bea7e63d8e3a0b52
Madhur Nutulapati <madhurnutulapati@gmail.com>
2023-03-12 01:27:10 -0500

    INTERACTIVE MODE PROGRESS but fixing+checking need

--- mysh.c
+++ mysh.c
@@ -4,19 +4,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-    //arbitrary value for the max command line characters
-    #define BUFF_SIZE 1024
+//arbitrary value for the max command line characters
 
-void runBatch(char *fName);
+#define BUFF_SIZE 1024
+
+void batchMode(char *fName);
+void interactiveMode();
+void introTag(int key);
 
 int main(int argc, char *argv[]){   
     if(argc==2){
         //batchmode
         //passes in file name to runBatch
-        runBatch(argv[1]);
+        batchMode(argv[1]);
     }
     if(argc==1){
         //interactive mode
+        interactiveMode();
     }
     if(argc==0){
         printf("No arguments passed in");
@@ -25,8 +29,8 @@ int main(int argc, char *argv[]){
 
     return 0; 
 }
-
-void runBatch(char *fName){
+//________________________________________________________________________________
+void batchMode(char *fName){
     
     int FD = open(fName,O_RDONLY);
 
@@ -35,7 +39,7 @@ void runBatch(char *fName){
     exit(1);
     }
 
-    ssize_t bytes;
+    ssize_t nBytes;
 
     char buffer[BUFF_SIZE];
 
@@ -43,26 +47,71 @@ void runBatch(char *fName){
     by newline characters, and executing each command as soon as 
     it is complete.
     */
-    while((bytes=read(FD,buffer,BUFF_SIZE))>0){
-        for(long int j=0; j<bytes;j++){
-            if(buffer[j]=='\n'){
-                buffer[j]='\0';
+    while((nBytes=read(FD,buffer,BUFF_SIZE))>0){
+            if(buffer[nBytes-1]=='\n'){
+                buffer[nBytes-1]='\0';
                 //function call to parse the commands  
                 //right here
-
-                buffer[j]='\n';
             }
-        }
-        write(STDOUT_FILENO, buffer, bytes);
+    
+        //write(STDOUT_FILENO, buffer, nBytes);
 
         //buffer command is the word exit
-        if (strcmp(buffer, "exit\n") == 0){break;}
+        if (strcmp(buffer, "exit\n") == 0){
+            close(FD);
+            return;
+        }
+
     }
         
-    if (bytes == -1) {
+    if (nBytes == -1) {
     perror("Error reading file");
     exit(1);
     }
 
     close(FD);
+}
+//________________________________________________________________________________
+void interactiveMode(){
+    printf("Welcome to mysh!\n");
+
+    //indicates to the print
+    int currPoint=0;
+
+    while(1){
+        //print command
+        introTag(currPoint); 
+
+        char buffer[BUFF_SIZE];
+        //read to standard input
+        int nBytes = read(STDIN_FILENO, buffer, BUFF_SIZE);
+
+        if(nBytes==0){
+            //exit while loop
+            break;
+        }
+                // Remove trailing newline
+        if (buffer[nBytes-1] == '\n') {
+            buffer[nBytes-1] = '\0';
+        }
+
+        // Execute command
+        //Make this command
+        //currPoint = execute_command(buffer);
+
+        // Check for exit command
+        if (strcmp(buffer, "exit") == 0) {
+            return;
+        }
+    }
+}
+//________________________________________________________________________________
+void introTag(int key){
+    if (key != 0) {
+        write(STDOUT_FILENO, "!mysh> ", 7);
+    } 
+    
+    else {
+        write(STDOUT_FILENO, "mysh> ", 6);
+    }
 }
